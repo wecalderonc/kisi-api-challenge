821dd544932628a7fef3efc35d896ae065934083
wcastillogc <97907331+wcastillogc@users.noreply.github.com>
2024-01-04 23:21:45 -0500

    increase chance of failure

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -1,11 +1,11 @@
 class TestJob < ApplicationJob
   def perform(data)
-    raise("Fake Failure") if rand < 0.2 # 20% chance of failure
+    raise("Fake Failure") if rand < 0.7 # 70% chance of failure
 
     # Job time duration
     sleep_duration = rand(5)
     sleep(sleep_duration)
 
-    Rails.logger.info("Processed TestJob with ID #{self.job_id}: data: #{data}, Sleep Duration: #{sleep_duration}s")
+    Rails.logger.info("Processed TestJob with ID #{job_id}: data: #{data}, Sleep Duration: #{sleep_duration}s")
   end
 end
